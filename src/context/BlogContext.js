babc768f323bc4c0a6126ded3b7a04e387dec417
DESKTOP-BD1HNTD\Mark <markandrew.colocado@gmail.com>
2023-07-26 18:44:13 +0800

    Refactor BlogContext Provider to use useReducer instead of useState.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    case "delete_blogpost":
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+  // const [blogPosts, setBlogPosts] = useState([]);
   // const blogPosts = [{ title: "Blog Post #1 " }, { title: "Blog Post #2 " }];
 
+  // const addBlogPost = () => {
+  //   setBlogPosts([
+  //     ...blogPosts,
+  //     { title: `Blog Post #${blogPosts.length + 1}` },
+  //   ]);
+  // };
+
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` },
-    ]);
+    dispatch({ type: "add_blogpost" });
   };
 
   return (
