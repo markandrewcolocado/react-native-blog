e7611a16f2242b21ab5009a5b01ef846601a38c2
DESKTOP-BD1HNTD\Mark <markandrew.colocado@gmail.com>
2023-07-26 10:41:45 +0800

    Adding context and accessing it on a nested child.l

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const value = useContext(BlogContext);
+  console.log(value);
   return (
     <View>
       <Text>Index Screen</Text>
